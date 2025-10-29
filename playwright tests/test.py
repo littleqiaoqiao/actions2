85731acedb25f86c37ab3726afa52d48e3808d2f
Sida Cheng <chengsidastar@gmail.com>
2025-10-28 22:26:03 -0400

    test committing local branch

---
+++ playwright tests/test.py
@@ -0,0 +1,10 @@
+from playwright_sync_api import sync_playwright
+
+with sync_playwright() as p:
+
+    browser = p.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://google.com")
+    context.close()
+    browser.close()
